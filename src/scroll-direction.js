eb1e692920c83378b85123ec421412feaf9d572e
Antonio Gallo <agx@linux.it>
2019-10-28 21:33:56 +0100

    added $scrollDetector

---
+++ src/scroll-direction.js
@@ -0,0 +1,39 @@
+
+function $scrollDetector(offset) {
+
+	var lastPosition = window.pageYOffset;
+	//var offset = 100;
+	var lastEvent = '';
+	offset = offset ? offset : 100;
+
+	document.addEventListener('scroll', function (ev) {
+		var currentPosition = window.pageYOffset;
+		var delta = Math.abs(currentPosition - lastPosition);
+		if (delta > offset) {
+			var direction = currentPosition > lastPosition ? 'down' : 'up';
+			lastPosition = currentPosition;
+			// Generate Event
+			return doScrollEvent(direction);
+		}
+		// Scroll on top only after at least one event fired
+		if (currentPosition === 0) {
+			lastPosition = currentPosition;
+			return doScrollEvent('top');
+		}
+		// Hit the bottom
+		var documentHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+		if (window.pageYOffset + window.innerHeight + offset > documentHeight) {
+			lastPosition = currentPosition;
+			return doScrollEvent('bottom');
+		}
+	});
+
+	function doScrollEvent(eventName) {
+		if (lastEvent === eventName) {
+			return;
+		}
+		lastEvent = eventName;
+		$triggerEvent(window.document, 'scroll-' + eventName);
+	}
+
+}
