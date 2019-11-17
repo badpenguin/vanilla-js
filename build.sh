654f324d59045a4eeb7d71f6e83561164aa61f7f
Antonio Gallo <agx@linux.it>
2019-11-17 18:33:55 +0100

    added $scrollDetector

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@
 closure-compiler \
     --js src/core.js \
     --js src/lightbox.js  --js src/lazyload.js   --js src/copyright.js \
-    --js src/visibility.js \
+    --js src/visibility.js src/scroll-direction.js \
     --js_output_file docs/vanilla-js.min.js --create_source_map docs/vanilla-js.min.js.map
 
 cp docs/vanilla-js.min.js* dist/
