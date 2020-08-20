a024eb5d0eee8e851f28e0e0813606721dd4f294
Antonio Gallo <agx@linux.it>
2020-08-20 13:29:20 +0200

    wip

--- src/ajax.js
+++ src/ajax.js
@@ -10,8 +10,9 @@
  * @param {string} url
  * @param {object} options
  * @param {function} callback
+ * @return {XMLHttpRequest}
  */
-var $ajax = function(method, url, options, callback) {
+var $ajax = function (method, url, options, callback) {
 
 	var request = new XMLHttpRequest();
 	request.open(method, url, true);
@@ -30,97 +31,110 @@ var $ajax = function(method, url, options, callback) {
 	}
 
 	var data = options.data || null;
+	var has_debug = options.debug || false;
+	var responseType = options.responseType || false;
 
 	// TODO automatically turn on if data is an instance of string?
-	if (options.sendUrlencoded) {
+	if (options['sendUrlencoded']) {
 		request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 		// Convert Object to String
 		// https://developer.mozilla.org/en-US/docs/Learn/Forms/Sending_forms_through_JavaScript
 		var tempdata = [];
-		for( var k1 in data ) {
+		for (var k1 in data) {
 			if (!data.hasOwnProperty(k1)) {
 				continue;
 			}
-			tempdata.push( encodeURIComponent( k1 ) + '=' + encodeURIComponent( data[k1] ) );
+			tempdata.push(encodeURIComponent(k1) + '=' + encodeURIComponent(data[k1]));
 		}
 		// Combine the pairs into a single string and replace all %-encoded spaces to
 		// the '+' character; matches the behaviour of browser form submissions.
-		data = tempdata.join( '&' ).replace( /%20/g, '+' );
-		console.warn('new data form urlencoded',data);  // TODO
+		data = tempdata.join('&').replace(/%20/g, '+');
 	}
 
 	// TODO
 	// TODO automatically turn on if data is an instance of FormData?
-	if (options.sendFormData) {
+	if (options['sendFormData']) {
 		request.setRequestHeader('Content-Type', 'multipart/form-data'); // TODO: charset?
 		// TODO is "data" instance of FormData ?
 		// TODO convert object to new formData ?
 	}
 
 	// TODO automatically turn on if data is an object?
-	if (options.sendJson) {
+	if (options['sendJson']) {
 		request.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
 		data = JSON.stringify(data);
-		console.warn('new data json',data); // TODO
+		if (!responseType) {
+			responseType = 'json';
+		}
 	}
 
-
 	if (options.headers) {
-		console.warn('TODO headers');
+		console.warn('TODO headers'); // TODO
 		for (var k2 in options.headers) {
 			if (!options.headers.hasOwnProperty(k2)) {
 				continue;
 			}
-			console.warn('k,v', k2, options.headers[k2]);
-			request.setRequestHeader(k2,options.headers[k2]);
+			console.warn('k,v', k2, options.headers[k2]); // TODO
+			request.setRequestHeader(k2, options.headers[k2]);
 		}
 	}
 
-	if (getObject(request,'upload.onprogress')) {
-		console.warn('hook upload'); // TODO
-		if (options.onprogress) {
-			request.upload.onprogress = function(pev) {
+	if (options.onprogress) {
+		if (getObject(request, 'upload.onprogress')) {
+			console.warn('hook upload'); // TODO
+
+			request.upload.onprogress = function (pev) {
 				var perc = Math.round((pev.loaded / pev.total) * 100);
 				options.onprogress(perc, pev);
 			}
+
+		} else {
+			console.warn('$ajax.js: no upload hook available');
 		}
-	} else {
-		console.warn('no upload'); // TODO
 	}
 
-	request.onreadystatechange = function() {
-		console.log('[$ajax] onreadystatechange', request.readyState, request.status, request.statusText);
+	if (responseType) {
+		request.responseType = responseType;
+	}
+
+	request.onreadystatechange = function () {
+		if (has_debug) {
+			console.debug('[$ajax] onreadystatechange', request.readyState, request.status, request.statusText);
+		}
 	};
 
-	request.onload = function() {
-		// TODO: valid status
-		console.log('[$ajax] onload', request.status, request.statusText);
-		callback(true, request);
+	request.onload = function () {
+		if (has_debug) {
+			console.log('[$ajax] onload', request.status, request.statusText);
+		}
+		var success = request.status>=200 && request.status<300;
+		callback(success, request);
 	};
 
-	request.onabort = function() {
+	request.onabort = function () {
 		// There was a connection error of some sort
-		console.log('[$ajax] onabort', request.status, request.statusText);
+		console.warn('[$ajax] onabort', request.status, request.statusText);
 		callback(false, request);
 	};
 
-	request.onerror = function() {
+	request.onerror = function () {
 		// There was a connection error of some sort
 		console.error('[$ajax] onerror', request.status, request.statusText);
 		callback(false, request);
 	};
 
-	request.ontimeout = function() {
+	request.ontimeout = function () {
 		console.error('[$ajax] ontimeout', request.status, request.statusText);
 		callback(false, request);
 	};
-
 
 	if (options.data) {
 		request.send(data);
 	} else {
 		request.send();
 	}
+
+	return request;
 };
 
 // TODO: append time to get request to avoid cache (new Date()).getTime());
