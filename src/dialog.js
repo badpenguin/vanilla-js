b0faf302cd5faf8707ee8c7b9bb50961d9ba68ba
Antonio Gallo <agx@linux.it>
2021-03-26 11:51:14 +0100

    $dialog, allow non blocking scroll for GDPR stuffs

--- src/dialog.js
+++ src/dialog.js
@@ -1,4 +1,3 @@
-
 /**
  * https://stackoverflow.com/questions/5999998/check-if-a-variable-is-of-function-type/6000009
  * @param {Function} callback
@@ -27,6 +26,7 @@ var $dialog = (function () {
 	var oldPosition = '';
 	var oldWidth = '';
 	var oldBehavior = '';
+	var allowScroll = false;
 
 
 	/**
@@ -46,16 +46,17 @@ var $dialog = (function () {
 		var spinner = settings.spinner || '';
 		var disableClose = settings.disableClose || false;
 		promiseCallback = settings.promiseCallback || null;
+		allowScroll = !!settings.allowScroll;
 
 		// CREATE MAIN DOM
 		targetEl = $id(selector);
 		if (!targetEl) {
 			elDialogInnerWindow = document.createElement('div');
-			$addClass(elDialogInnerWindow, 'dialog-window__inner' );
+			$addClass(elDialogInnerWindow, 'dialog-window__inner');
 
 			elCloseButton = document.createElement('button');
 			$addClass(elCloseButton, 'dialog-window__close');
-			elCloseButton.innerHTML='&times;'
+			elCloseButton.innerHTML = '&times;'
 
 			var elDialogWindow = document.createElement('div');
 			$addClass(elDialogWindow, 'dialog-window');
@@ -63,7 +64,7 @@ var $dialog = (function () {
 			elDialogWindow.appendChild(elDialogInnerWindow);
 
 			targetEl = document.createElement('div');
-			targetEl.setAttribute('id',selector);
+			targetEl.setAttribute('id', selector);
 			$addClass(targetEl, 'dialog-backdrop');
 			targetEl.appendChild(elDialogWindow);
 
@@ -82,16 +83,16 @@ var $dialog = (function () {
 				ev.stopPropagation();
 			});*/
 
-			elCloseButton.addEventListener('click',function(ev) {
+			elCloseButton.addEventListener('click', function (ev) {
 				ev.preventDefault();
 				ev.stopPropagation();
 				self.hide();
 			});
 
 			// Attach backgrop event
-			targetEl.addEventListener('click',function(ev){
+			targetEl.addEventListener('click', function (ev) {
 				// user clicked on a child and we ignore that
-				if ( ev.target !== ev.currentTarget ){
+				if (ev.target !== ev.currentTarget) {
 					return;
 				}
 				ev.preventDefault();
@@ -113,7 +114,7 @@ var $dialog = (function () {
 		// Reset class
 		targetEl.className = 'dialog-backdrop';
 		if (className) {
-			$addClass(targetEl,className);
+			$addClass(targetEl, className);
 		}
 
 		if (disableClose) {
@@ -127,32 +128,32 @@ var $dialog = (function () {
 
 		if (title) {
 			var elTitle = document.createElement('div');
-			$addClass(elTitle, 'dialog-window__title' );
+			$addClass(elTitle, 'dialog-window__title');
 			elTitle.innerHTML = title;
 			elDialogInnerWindow.appendChild(elTitle);
 		}
 
 		if (content) {
 			var elContent = document.createElement('div');
-			$addClass(elContent, 'dialog-window__content' );
+			$addClass(elContent, 'dialog-window__content');
 			elContent.innerHTML = content;
 			elDialogInnerWindow.appendChild(elContent);
 		}
 
 		if (spinner) {
 			var elSpinner = document.createElement('div');
-			$addClass(elSpinner, 'dialog-window__spinner' );
-			$addClass(elSpinner, spinner );
+			$addClass(elSpinner, 'dialog-window__spinner');
+			$addClass(elSpinner, spinner);
 			elDialogInnerWindow.appendChild(elSpinner);
 		}
 
-		if (buttons.length>0) {
+		if (buttons.length > 0) {
 
 			var btnContainer = document.createElement('div');
-			$addClass(btnContainer, 'dialog-window__buttons' );
+			$addClass(btnContainer, 'dialog-window__buttons');
 			elDialogInnerWindow.appendChild(btnContainer);
 
-			for (var i=0;i<buttons.length;i++) {
+			for (var i = 0; i < buttons.length; i++) {
 				var btn = buttons[i];
 				if ($isString(btn)) {
 					btn = {
@@ -160,15 +161,15 @@ var $dialog = (function () {
 					}
 				}
 				var btnLabel = btn.label || 'OK';
-				var btnClass = btn.className || (i==0 ? 'btn--primary btn--large' : 'btn--secondary');
+				var btnClass = btn.className || (i == 0 ? 'btn--primary btn--large' : 'btn--secondary');
 				var btnValue = btn.value || btn.label;
 				// TODO: advanced callback
 
 				// Attach
 				var btnEl = document.createElement('button');
-				btnEl.className = 'btn '+ btnClass;
+				btnEl.className = 'btn ' + btnClass;
 				btnEl.innerHTML = btnLabel;
-				btnEl.onclick = this.hide.bind(this,btnValue);
+				btnEl.onclick = this.hide.bind(this, btnValue);
 				btnContainer.appendChild(btnEl);
 			}
 		}
@@ -190,14 +191,16 @@ var $dialog = (function () {
 		open = false;
 
 		// Restore Document
-		//scrollPos = parseInt(document.body.style.top || '0');
-		document.body.style.position = oldPosition;
-		document.body.style.width = oldWidth;
-		// Reset smooth after scroll
-		window.scrollTo(0, oldScrollTop);
-		document.documentElement.style.scrollBehavior = oldBehavior;
+		if (!allowScroll) {
+			//scrollPos = parseInt(document.body.style.top || '0');
+			document.body.style.position = oldPosition;
+			document.body.style.width = oldWidth;
+			// Reset smooth after scroll
+			window.scrollTo(0, oldScrollTop);
+			document.documentElement.style.scrollBehavior = oldBehavior;
+		}
 
-		$removeClass(targetEl,'open');
+		$removeClass(targetEl, 'open');
 
 		// Clear everything inside
 		$empty(elDialogInnerWindow);
@@ -221,16 +224,18 @@ var $dialog = (function () {
 		// DO THE SHOW
 		open = true;
 		// add class
-		$addClass(targetEl,'open');
+		$addClass(targetEl, 'open');
 		// Freeze Document
-		oldScrollTop = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
-		oldPosition = document.body.style.position;
-		oldWidth = document.body.style.width;
-		oldBehavior = document.documentElement.style['scroll-behavior'];
-		document.body.style.position = 'fixed';
-		document.body.style.top = '-' + oldScrollTop + 'px';
-		document.body.style.width = '100%';
-		document.documentElement.style.scrollBehavior = 'auto';
+		if (!allowScroll) {
+			oldScrollTop = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+			oldPosition = document.body.style.position;
+			oldWidth = document.body.style.width;
+			oldBehavior = document.documentElement.style['scroll-behavior'];
+			document.body.style.position = 'fixed';
+			document.body.style.top = '-' + oldScrollTop + 'px';
+			document.body.style.width = '100%';
+			document.documentElement.style.scrollBehavior = 'auto';
+		}
 	}
 
 
